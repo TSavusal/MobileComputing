90d10a3f56ec2855c7aa8873c5e56f9423b07f6b
tuomas.savusalo <Tuomas.Savusalo@student.oulu.fi>
2020-01-28 08:40:22 +0200

    HW3 V1

---
+++ app/src/main/res/layout/ReminderAdapter.kt
@@ -0,0 +1,23 @@
+package fi.oulu.reminder2020
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+    class ReminderAdapter : BaseAdapter() {
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun getItem(position: Int): Any {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun getItemId(position: Int): Long {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun getCount(): Int {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }
